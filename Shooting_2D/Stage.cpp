2d79353aa9c8f2fbdf6a85e381c36463f30b6df7
T K <blue.bit0501@gmail.com>
2017-02-03 17:08:26 +0900

    不要なファイルの削除

--- Shooting_2D/Stage.cpp
+++
@@ -1,9 +0,0 @@
-#include "Stage.h"
-
-Stage::Stage()
-{
-}
-
-void Stage::All()
-{
-}
--- Shooting_2D/Stage.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Stage {
-	int bgHandle;	// ”wŒi—p‚Ì‰æ‘œƒnƒ“ƒhƒ‹
-
-public:
-	Stage();
-	void All();
-};
