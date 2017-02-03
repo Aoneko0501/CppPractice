62d4d42a65f82df95167a82546305f4dcb06a976
T K <blue.bit0501@gmail.com>
2017-02-04 00:15:14 +0900

    [Update]JoyPadのPOV（方向キー）を認識するように

--- 2DShootingGame/DragonShooter.cpp
+++ 2DShootingGame/DragonShooter.cpp
@@ -1,1 +1,16 @@
 #include"DxLib.h"
+using namespace std;
+
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); // ウィンドウモード変更、初期化、裏画面設定
+
+	int pad; // ジョイパッド
+	int pov;
+
+	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen()) {
+
+	}
+
+	DxLib_End();
+	return 0;
+}
---
+++ 2DShootingGame/KeyInput.cpp
@@ -0,0 +1,15 @@
+/*
+pad = GetJoypadInputState(DX_INPUT_KEY_PAD1);
+pov = GetJoypadPOVState(DX_INPUT_PAD1, 0);
+for (int i = 0; i < 28; i++) {
+if (pad & (1 << i)) {
+DrawFormatString(0, i * 15, GetColor(255, 255, 255), "%dのキーが押されています。",i);
+}
+}
+// 十字キー確認
+for (int i = 28; i < 32; i++) {
+if (pov == (31 - i) * 9000) {
+DrawFormatString(0, (i-10) * 15, GetColor(255, 255, 255), "%dのキーが押されています。", i);
+}
+}
+*/
